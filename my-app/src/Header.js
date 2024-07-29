38eb1431238fd87f69368eb5d1f1f3d879e8bfbc
jlounsb1 <jefflounsbury@gmail.com>
2024-07-29 12:47:01 -0400

    created new branch and made react app, created new component for header and got it to render

---
+++ my-app/src/Header.js
@@ -0,0 +1,27 @@
+
+
+// This is the old header I want to import as a module.
+
+
+function Header() {
+    return (
+        <>
+        <header>
+            <h1>Sartre's List</h1>
+            <h3>Better-Dressed People</h3>
+            <nav aria-label="Main Navigation" role="navigation">
+                <ul>
+                <li><a href="...">Women's</a></li>
+                <li><a href="...">Men's</a></li>
+                <li><a href="...">On the Street</a></li>
+                <li><a href="...">The Catwalk</a></li>
+                <li><a href="...">AdWatch</a></li>
+                <li><a href="...">About</a></li>
+                </ul>
+            </nav>
+        </header>
+        </>
+    )
+}
+
+export default Header;
